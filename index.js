5e2535c10a6f6aee03cb2081b09c5cf6ada0db3c
Andrew Tate <tate@gmail.com>
2023-07-17 22:03:33 +0300

    save

--- index.js
+++ index.js
@@ -5,9 +5,33 @@ const moment = require('moment');
 const cors = require('cors')
 require('dotenv').config()
 const XlsxPopulate = require('xlsx-populate');
+var convertapi = require('convertapi')('BpMFm93JuS1vLLrV');
+const fs = require('fs');
 
 app.use(cors())
+app.use(express.json());
 
+const DOCUMENTS_KEY = 'documents';
+const getPayloadLocations = (inputData) => {
+    const {
+      delivery,
+      admission,
+      customdelivery,
+      customadmission
+    } = inputData;
+  
+    const deliveryText = delivery === 3
+      ? customdelivery
+      : LOCATIONS_MAP[delivery];
+    const admissionText = admission === 3
+      ? customadmission
+      : LOCATIONS_MAP[admission];
+  
+    return {
+      delivery: deliveryText,
+      admission: admissionText
+    };
+  };
 const carMap = {
     1: 'TOYOTA RAV4 AWD',
     2: 'KIA SPORTAGE AWD',
@@ -62,12 +86,10 @@ admin.initializeApp({
 });
 const db = admin.firestore();
 
-app.use(express.json());
-
-var convertapi = require('convertapi')('BpMFm93JuS1vLLrV');
 const convertToPdf = async (id) => {
     const x = convertapi.convert('pdf', {
-        File: `./xlsx/${id}-12_11-07-2029_Yefim.xlsx`
+        File: `./xlsx/${id}.xlsx`,
+        WorksheetIndex: 2
     }, 'xls').then(function(result) {
         const y = result.saveFiles('./pdf/');
 
@@ -77,40 +99,51 @@ const convertToPdf = async (id) => {
     return x;
 };
 
-app.get('/pdf', (req, res) => {
-    const id = req.body.id || 0;
-    const name = `${id}-12_11-07-2029_Yefim`;
+app.post("/downloadPdfById", async (req, res) => {
+    const name = `${req.body.contractNumber.replace("/", "-")}`;
+    const path = `./pdf/${name}.pdf`;
+    try {
+        if (fs.existsSync(path)) {
+            res.download(path);
+        } else {
+            res.sendStatus(404);
+        }
+    } catch(err) {
+        res.sendStatus(404);
+    }
+});
+
+app.post('/pdf', (req, res) => {
+    const {
+        contractNumber,
+        from,
+        to,
+        delivery,
+        admission
+    } = req.body;
+
+    const name = `${contractNumber.replace("/", "-")}`;
     const x = XlsxPopulate.fromFileAsync("./template.xlsx")
         .then(workbook => {
-            const value = workbook.sheet(0).cell("C12").value(25);
+            // dates/locations
+            workbook.sheet(0).cell("C2").value(contractNumber);
+            workbook.sheet(0).cell("C3").value(from);
+            workbook.sheet(0).cell("C4").value(to);
+            workbook.sheet(0).cell("C5").value(delivery);
+            workbook.sheet(0).cell("C6").value(admission);
 
             return workbook.toFileAsync(`./xlsx/${name}.xlsx`);
         });
 
     x.then(_ => {
-        convertToPdf(id).then(_ => res.download(`./pdf/${name}.pdf`))
+        convertToPdf(name).then(_ => res.sendStatus(200))
     })
 })
 
-app.get("/api/list", async (_, res) => {
-    let applications = [];
-
-    const byCollectionRef = db.collection('ez');
-    const snapshot = await byCollectionRef.get();
-    snapshot.forEach(doc => {
-        applications.push({
-            id: doc.id,
-            ...doc.data()
-        });
-    });
-
-    res.send(applications);
-});
-
 app.post('/api/create', async function (req, res) {
     const payload = {
-        "from": moment(req.body.from).format('DD.MM.YYYY') || null,
-        "to": moment(req.body.to).format('DD.MM.YYYY') || null,
+        "from": moment(req.body.from, 'YYYY-MM-DD').format('DD.MM.YYYY') || null,
+        "to": moment(req.body.to, 'YYYY-MM-DD').format('DD.MM.YYYY') || null,
         "username": req.body.username || null,
         "tel": req.body.tel || null,
         "admission": req.body.admission || null,
@@ -143,9 +176,93 @@ app.post('/api/create', async function (req, res) {
         });
     });
     const id = applications.length;
-    await byCollectionRef.doc(id.toString()).set(payload);
+    await byCollectionRef.doc(id.toString()).set({
+        ...payload,
+        id
+    });
 
     res.status(200).send(payload);
+})
+
+app.post('/api/createDoc', async function (req, res) {
+    console.log(req.body)
+    const payload = {
+        ...req.body,
+        "carModel": carMap[req.body.carId] || req.body.carId || null,
+        "carMaxSpeed": 130 || null,
+        "otherDeposit": 300 || null,
+        "otherFuelLevel": '100%' || null,
+    };
+
+    if (
+        !req.body.from ||
+        !req.body.to ||
+        !req.body.car ||
+        !req.body.username ||
+        !req.body.tel
+    ) {
+        return res.status(400).send('One of the required fields is empty');
+    }
+
+    const byCollectionRef = db.collection(DOCUMENTS_KEY);
+    let documents = [];
+    const snapshot = await byCollectionRef.get();
+    snapshot.forEach(doc => {
+        documents.push({
+            id: doc.id,
+            ...doc.data()
+        });
+    });
+
+    const max = documents.reduce((acc, val) => {
+        return acc.id > val.id ? acc : val;
+    }, []);
+
+    const idNumber = Number(max.id) || documents.length.toString();
+    const id = Number(idNumber) + 1;
+
+    await byCollectionRef.doc(`RDV-${id}`).set({
+        ...payload,
+        id
+    });
+
+    res.status(200).send({
+        ...payload,
+        id
+    });
+})
+
+app.post('/api/saveDoc', async function (req, res) {
+    // const locations = getPayloadLocations(req.body);
+    const payload = {
+        ...req.body,
+    };
+
+    const byCollectionRef = db.collection(DOCUMENTS_KEY);
+    await byCollectionRef.doc(`RDV-${req.body.id}`).set(payload);
+    res.status(200).send(payload);
+})
+
+app.get('/api/getDoc', async function (req, res) {
+    const { id } = req.query;
+
+    let documents = [];
+
+    const byCollectionRef = db.collection(DOCUMENTS_KEY);
+    const snapshot = await byCollectionRef.get();
+    snapshot.forEach(doc => {
+        documents.push({
+            ...doc.data()
+        });
+    });
+    const document = documents.find(doc => doc.id == id);
+
+    if (!document) {
+        res.status(400).send(`Document with ID: ${id} is not found`);
+        return;
+    }
+
+    res.status(200).send(document);
 })
 
 app.listen(process.env.PORT || 80, async() => {
